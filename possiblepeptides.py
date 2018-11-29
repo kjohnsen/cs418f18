cd232e9959fe526f0011f8fd1ec91732ddf5fbff
Kyle <johnsenkyle13@gmail.com>
2018-11-29 12:55:27 -0700

    Peptide stuff

--- possiblepeptides.py
+++ possiblepeptides.py
@@ -1,7 +1,7 @@
 from aamass import aa_mass
 
 def num_possible_peptides_for_mass(mass):
-    aa_masses = aa_mass.values()
+    aa_masses = list(aa_mass.values())
     count_array = []
     for i in range(mass + 1):
         possible_peptides_count = 0
@@ -9,8 +9,13 @@ def num_possible_peptides_for_mass(mass):
             if aa_masses.__contains__(i):
                 possible_peptides_count += 1
         for am in aa_masses:
-            try:
-                count += count_array[]
+            if i - am >= 0:
+                if i < 200 and i > 186: print(count_array)
+                possible_peptides_count += count_array[i - am]
+                if i < 200 and i > 186: print(possible_peptides_count)
+        count_array.append(possible_peptides_count)
+    return count_array[-1]
+
 
 
 
